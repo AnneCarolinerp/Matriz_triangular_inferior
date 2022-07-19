f9c2950715dd5ad6c746a30a54df2f4b1220f956
Carol Rodrigues <108235466+AnneCarolinerp@users.noreply.github.com>
2022-07-18 22:40:37 -0300

    Update headquarters.py

--- headquarters.py
+++ headquarters.py
@@ -3,13 +3,16 @@
 
 #Transformando M1 em uma matriz triangular inferior, atribuindo zero a todos os elementos acima da diagonal principal.
 #M1
+import numpy as np
 def triangular_inferior(m1):
   for linha in range(3):
     for coluna in range(3):
       if(linha < coluna):
         m1[linha][coluna] = 0
   return m1
-print(triangular_inferior(m1))
-print("\n",m2)
+
 m1 = np.array([[1, 2, 3], [5, 6, 7], [8, 9, 0]])
 m2 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+
+print(triangular_inferior(m1))
+print("\n",m2)
